650001891e9ca5c006f4d629849b2f9c5261db5b
Karan Thukral <karanthukral@gmail.com>
2022-02-04 14:19:50 -0500

    try

--- packages/default/hello.js
+++ packages/default/hello.js
@@ -1,7 +1,6 @@
 function main(args) {
     let name = args.name || 'stranger'
-    let greeting = 'Hello ' + process.env.USER + '!'
+    let greeting = 'Hello ' + process.env.USER + + 'from' + args["run_env"] + 'env!'
     console.log(greeting)
     return {"body": greeting}
   }
-  
